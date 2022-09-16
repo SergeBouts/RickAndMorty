5a763e03fc63146ab03a145debfa9ca763a16e45
Serge Bouts <sergebouts@gmail.com>
2022-09-16 15:53:45 -0400

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Serge Bouts on 2022-09-16.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
